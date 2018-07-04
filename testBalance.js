b05d3cee02ba3762dedb506b8339dd3777244c74
sol wu <apiswswsw@gmail.com>
2018-07-04 02:58:43 -0700

    testing if we can get balance from inside the contract

---
+++ testBalance.js
@@ -0,0 +1,31 @@
+/**
+ * test to see if we can get balance from inside the contract
+ */
+class Test {
+  constructor() {
+  }
+
+  init() {
+  }
+
+  deposit() {
+    
+  }
+
+  balance() {
+    `let contractAddr = Blockchain.transaction.to;
+    return Blockchain.getAccountState(contractAddr).balance;`
+  }
+
+  contractState() {
+    let contractAddr = Blockchain.transaction.to;
+    return Blockchain.getAccountState(contractAddr);
+  }
+
+  txInfo() {
+    return Blockchain.transaction;
+  }
+
+}
+
+module.exports = Test;
